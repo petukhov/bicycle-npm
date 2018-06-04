f1132a8f5c3ab776c5a14269918abcbdeb5e96d9
Georgy Petukhov <gpetukhov@me.com>
2018-06-05 00:32:43 +0800

    first commit

---
+++ index.ts
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+interface PackageDeclaration {
+    name: string,
+    version: string,
+}
+
+const parsePackageJson = (): PackageDeclaration[] => {
+    return [
+        {
+            name: 'test',
+            version: '123'
+        }
+    ];
+}
+
+const downloadPackages = async function (packages: PackageDeclaration[]) {
+    const result = await axios.get('https://google.com', {});
+    console.log(result.data);
+}
+
+const packages = parsePackageJson();
+downloadPackages(packages);
+
+console.log('the app is running');
+

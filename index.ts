36df7e2ad4c7510a610ad39c399bd70084063c41
Georgy Petukhov <gpetukhov@me.com>
2018-06-05 00:41:31 +0800

    changed indentation

--- index.ts
+++ index.ts
@@ -1,22 +1,22 @@
 import axios from 'axios';
 
 interface PackageDeclaration {
-    name: string,
-    version: string,
+  name: string,
+  version: string,
 }
 
 const parsePackageJson = (): PackageDeclaration[] => {
-    return [
-        {
-            name: 'test',
-            version: '123'
-        }
-    ];
+  return [
+    {
+      name: 'test',
+      version: '123'
+    }
+  ];
 }
 
 const downloadPackages = async function (packages: PackageDeclaration[]) {
-    const result = await axios.get('https://google.com', {});
-    console.log(result.data);
+  const result = await axios.get('https://google.com', {});
+  console.log(result.data);
 }
 
 const packages = parsePackageJson();

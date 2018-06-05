6789871e8df83df94ca75466119d27b5dc500462
Georgy Petukhov <gpetukhov@me.com>
2018-06-06 00:36:50 +0800

    wrote get package declarations

--- index.ts
+++ index.ts
@@ -1,26 +1,38 @@
 import axios from 'axios';
+import fs from 'fs';
+import _ from 'lodash';
 
 interface PackageDeclaration {
   name: string,
   version: string,
 }
 
-const parsePackageJson = (): PackageDeclaration[] => {
-  return [
-    {
-      name: 'test',
-      version: '123'
-    }
-  ];
+const findPackageJson = (): string => {
+  // TODO: search for the package in current directory
+  return 'package.json';
+}
+
+const getPackageDeclarations = (depsStr: {[pckgName: string]: string}): PackageDeclaration[] => {
+  return _(depsStr)
+          .toPairs()
+          .map(pair => ({name: pair[0], version: pair[1]}))
+          .value();
+}
+
+const parsePackageJson = (path: string): PackageDeclaration[] => {
+  const contentsStr = fs.readFileSync(path, 'utf8');
+  const contents = JSON.parse(contentsStr);
+  const packages = getPackageDeclarations(contents.dependencies);
+  return packages;
 }
 
 const downloadPackages = async function (packages: PackageDeclaration[]) {
-  const result = await axios.get('https://google.com', {});
-  console.log(result.data);
+  //const result = await axios.get('https://google.com', {});
+  //console.log(result.data);
 }
 
-const packages = parsePackageJson();
+const path = findPackageJson();
+const packages = parsePackageJson(path);
 downloadPackages(packages);
 
-console.log('the app is running');
 

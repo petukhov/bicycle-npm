4f9e721e69b41742afc769abb137f8c168341abf
Georgy Petukhov <gpetukhov@me.com>
2018-06-06 14:46:16 +0800

    changed circleci config

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import { downloadPackages, findPackageJson, parsePackageJson } from './utils';
 
 console.log('the app is running');
+function a() {}
 const path = findPackageJson();
 const packages = parsePackageJson(path);
 downloadPackages(packages);
